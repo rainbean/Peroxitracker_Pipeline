1b19d9665c324167158b3e55aaa37a70bb90b645
Jimmy Lee <Jimmy.m.lee@gmail.com>
2014-04-14 12:10:12 +0800

    add template of list of plate insert

--- test3.js
+++ test3.js
@@ -1,7 +1,7 @@
 //var azure = require('azure');
 var S = require('string');
-var azure = require('azure-mgmt-compute');
 var fs = require('fs');
+var async = require('async');
 
 var g_storageAccount = 'bioimage';
 var g_storageAcessKey = 't2cCFG4nKcwSp4NrnghpI9fnZZ3hR8YvEYshRocCAzXJ5u3dSEx+b5sA05URmKk1MOFwVwStHa+d1la6TMauxA==';
@@ -22,10 +22,27 @@ g_queue.clearMessages('plates', function(err) {
 });
 */
 
-g_queue.createMessage('plates', 'PBD GFP-Hoechst_LOPAC_2_1', function(err) {
-  if (err) {
-    console.log('Failed to create message: ' + err);
-  }
+plates = [
+'H1098495', 
+'H1098497', 
+'H1098499', 
+'H1098501', 
+'H1098503', 
+'H1098505', 
+'H1098507', 
+'H1098509', 
+'H1098511', 
+'H1098513', 
+'H1098515'
+];
+
+async.eachSeries(plates, function (plate, callback) {
+  g_queue.createMessage('plates', plate, function(err) {
+    if (err) {
+      console.log('Failed to create message: ' + plate + ', error: ' + err);
+    }
+	callback();
+  });
 });
 
 /*
